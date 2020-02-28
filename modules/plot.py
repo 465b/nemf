b9f8a3fc188ad648dff23ee4658f208f2c41c985
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-02-28 15:36:40 +0100

    added optimisation outpus summary plot

--- modules/plot.py
+++ modules/plot.py
@@ -5,6 +5,7 @@ import networkx as nx
 
 sns.set_style('whitegrid')
 sns.set_context('paper')
+sns.set_palette("husl")
 
 # plotting routines
 
@@ -54,7 +55,97 @@ def interaction_graph(model_config):
            arrowsize=20)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels,
         label_pos=0.35, font_size=10,font_color='tab:red',rotate=False)
+
+
+def draw_cost(ax,cost):
+    ax.title.set_text('cost')
+    ax.set_ylabel('Cost function (a.u.)')
+    ax.set_xlabel('Iteration Step')
+    plt.plot(cost)
+
+def draw_predictions(ax,predictions,model_config):
+    if model_config.configuration['fit_model'] == 'direct_fit_model':
+        labels = list(model_config.states)
+    elif model_config.configuration['fit_model'] == 'net_flux_fit_model':
+        labels = ['net incoming/outgoing flux']
+    else:
+        labels = []    
+    ax.title.set_text('fit model prediction')
+    handles = plt.plot(predictions)
+    ax.set_ylabel('Model predictions (a.u.)')
+    ax.set_xlabel('Iteration Step')
+    plt.legend(handles, labels)
+
+
+def draw_parameters(ax,parameters,model_config):
+    labels = model_config.to_grad_method()[2]
+    ax.title.set_text('parameters')
+    handles = plt.plot(parameters)
+    ax.set_ylabel('optimized parameters (a.u.)')
+    ax.set_xlabel('Iteration Step')
+    plt.legend(handles, labels)
+
+def draw_model_output(ax,model,model_config):
+    dt = model_config.configuration['dt_time_evo']
+    T = model_config.configuration['time_evo_max']
+    time = np.arange(T,step=dt)
+    labels = list(model_config.states)
+
+    ax.title.set_text('optimized model')
+    handles = plt.plot(time,model)
+    plt.legend(handles, labels)
+    ax.set_ylabel('Model output (a.u.)')
+    ax.set_xlabel('Time (a.u.)')
+
+
+def optimization_overview(cost,predictions,parameters,model
+                            ,model_config,ii_sample=None): 
+
+    if ii_sample is None:
+        ii_sample = ''
+    else:
+        ii_sample = '#{}'.format(ii_sample)
+    fig = plt.figure()
+    fig.suptitle('Results of optimization run'+ii_sample)
+    
+    ax1 = plt.subplot(2,2,1)
+    draw_cost(ax1,cost[:-1])
+    ax2 = plt.subplot(2,2,2)
+    draw_predictions(ax2,predictions[:-1],model_config)
+    ax3 = plt.subplot(2,2,3)
+    draw_parameters(ax3,parameters[:-1],model_config)
+    ax4 = plt.subplot(2,2,4)
+    draw_model_output(ax4, model,model_config)
+
+    plt.tight_layout()
     plt.show()
+
+
+def output_summary(model_config):
+    log = model_config.log
+
+    sample_sets_switch = len(np.shape(log['parameters']))
+    if sample_sets_switch == 1:
+        # no optimization has happend.
+        # hence, cost/predictions/parameters is 0-dim
+        print(log['cost'])
+        print(log['predictions'])
+        print(log['parameters'])
+        ax = plt.subplot(1,1,1)
+        draw_model_output(ax,log['model'],model_config)
+    elif sample_sets_switch == 2:
+        optimization_overview(log['cost'],
+                   log['predictions'],
+                   log['parameters'],
+                   log['model'][0],
+                   model_config)
+    elif sample_sets_switch == 3:
+        for ii in np.arange(np.shape(log['parameters'])[0]):
+            optimization_overview(log['cost'][ii],
+                    log['predictions'][ii],
+                    log['parameters'][ii],
+                    log['model'][ii][-2],
+                    model_config)
 
 
 def coupling_matrix(d2_weights,ODE_coeff_weights,names):
