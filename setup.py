7cb24adbf851b856fc708227dfb1f2f8b6691dbb
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-04-24 18:40:46 +0200

    added setup of package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="gemf",
+    version="0.2.3",
+    packages=find_packages(),
+
+    # install_requires=[""],
+
+    # metadata to display on PyPI
+    author="Laurin Steidle",
+    author_email="laurin.steidle@uni-hamburg.de",
+    description="General Ecosystem Modelling Framework",
+    keywords="ecosystem modelling framework inverse-modelling",
+    url="https://github.com/465b/General-Ecosystem-Modeling-Framework/"
+    
+    # could also include long_description, download_url, etc.
+)
