7b451997833493a2bab292fa99a4782839f6f051
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-04-27 10:15:57 +0200

    rounded out setup file with additional description

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme_as_long_description = f.read()
+
 setup(
     name="gemf",
     version="0.2.3",
@@ -10,8 +16,19 @@ setup(
     author="Laurin Steidle",
     author_email="laurin.steidle@uni-hamburg.de",
     description="General Ecosystem Modelling Framework",
+    long_description=readme_as_long_description,
+    long_description_content_type="text/markdown",
     keywords="ecosystem modelling framework inverse-modelling",
-    url="https://github.com/465b/General-Ecosystem-Modeling-Framework/"
-    
-    # could also include long_description, download_url, etc.
+    url="https://github.com/465b/General-Ecosystem-Modeling-Framework/",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Development Status :: 3 - Alpha',
+    ],
+    license='BSD',
+    install_requires=[
+        'numpy','seaborn','matplotlib','networkx','pyyaml']
 )
