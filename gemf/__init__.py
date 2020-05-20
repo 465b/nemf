f5eabb5f17e036f98b4b7de5a2c0e42b82f2d0cf
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-05-20 20:54:22 +0200

    removed old time-evolution method from init

--- gemf/__init__.py
+++ gemf/__init__.py
@@ -9,7 +9,6 @@ from gemf.models import model_class as model_class
 
 from gemf.caller import forward_model as forward_model 
 from gemf.caller import inverse_model as inverse_model
-from gemf.caller import time_evolution as time_evolution
 
 from gemf.plot import output_summary as output_summary
 from gemf.plot import interaction_graph as interaction_graph
