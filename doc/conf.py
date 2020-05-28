690db35f8204e2cb62037b83daac7133414eed7d
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-05-28 18:43:14 +0200

    ready-up for readthedocs; added autodocing

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,7 +28,19 @@ author = 'Laurin Steidle'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	# For conversion from markdown to html
+	'recommonmark',
+	# For auto generating documentation from docstrings
+	'sphinx.ext.autodoc',
+	# For numpy style docstrings
+	'sphinx.ext.napoleon',
+	# Linking source code to functions
+	'sphinx.ext.viewcode',
 ]
+
+
+# Add type of source files
+source_suffix = ['.rst','.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,9 +56,30 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Laurin added this -------------------------------------------------------
+ 
+# Sphinx error:master file 
+# /home/docs/checkouts/readthedocs.org/user_builds/ ...
+#  ... general-ecosystem-modeling-framework/checkouts/latest/doc/
+#  ... contents.rst not found
+# to fix the error
+
+master_doc = 'index'
+
+autodoc_mock_imports = [
+	'numpy',
+	'seaborn',
+	'matplotlib',
+	'networkx',
+	'termcolor',
+	'scipy',
+	'yaml'
+	]
