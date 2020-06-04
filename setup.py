a7de6e8c33f9aa9ebd7986a08c8b5cca48858731
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-06-04 15:10:33 +0200

    updated: pickles; version numbers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="nemf",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
 
     # install_requires=[""],
