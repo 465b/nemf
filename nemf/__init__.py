794c3bb6844b4f84fefa5dee5ba6e1e6adb92ac6
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-06-16 14:14:56 +0200

    minor: added ref_data_path to load_model

--- nemf/__init__.py
+++ nemf/__init__.py
@@ -8,14 +8,18 @@ import nemf.interaction_functions
 
 from nemf.models import model_class as model_class
 
-def load_model(path):
+def load_model(model_path,ref_data_path):
 	""" Loads a model by reading its model configuration from from file
 
 	Parameters
 	----------
 	
-	path : string
+	model_path : string
 		path to yaml file containing the model
+	ref_data_path : string
+		path to file, plain text or xls(x), containing reference data to the 
+		model.
+		See: https://nemf.readthedocs.io/en/latest/README_reference_data.html
 
 	Returns
 	-------
@@ -25,7 +29,7 @@ def load_model(path):
 	
 	"""
 
-	return model_class(path)
+	return model_class(model_path,ref_data_path)
 
 
 from nemf.caller import forward_model as forward_model 
