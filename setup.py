e1b059083d694aa116320b889c7ba213b4e26ca6
Laurin Steidle <laurin.steidle@univie.ac.at>
2020-06-23 16:39:36 +0200

    package: updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="nemf",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
 
     # install_requires=[""],
